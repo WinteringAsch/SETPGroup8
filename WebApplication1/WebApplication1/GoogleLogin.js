2491aa1582fb584ed014dc7cba65d11287cd9067
용우 이 <dldyddn31@naver.com>
2018-06-01 10:57:05 +0900

    되돌리기를 사용했습니다.

--- WebApplication1/WebApplication1/GoogleLogin.js
+++ WebApplication1/WebApplication1/GoogleLogin.js
@@ -9,45 +9,53 @@
         messagingSenderId: "380923923080"
     };
     firebase.initializeApp(config);
-    var provider = new firebase.auth.GoogleAuthProvider();
 
     //Get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
     const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
     const btnLogout = document.getElementById('btnLogout');
 
     //Add login event
     btnLogin.addEventListener('click', e => {
-        window.alert("Login btn");
-        
-        firebase.auth().signInWithRedirect(provider);
-        firebase.auth().getRedirectResult().then(function (result) {
-            if (result.credential) {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = result.credential.accessToken;
-                // ...
-            }
-            // The signed-in user info.
-            var user = result.user;
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
+        //Get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        //Sign in
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
+    //Add signup event
+    btnSignUp.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        //Sign in
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+        promise
+            .catch(e => console.log(e.message));
     });
 
     btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut().then(function () {
-            window.alert("log out success");
-            // Sign-out successful.
-        }).catch(function (error) {
-            window.alert("log out failed");
-            // An error happened.
-        });
+        firebase.auth().signOut();
+
+    });
+
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser);
+            //btnLogout.classList.remove('hide');
+        } else {
+            console.log('not logged in');
+            //btnLogout.classList.add('hide');
+        }
+
     });
 
 }());
