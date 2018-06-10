86df38c4a95feacbe8ccff180b4d7564f8283684
Junhyeon Moon <paranocs@gmail.com>
2018-06-10 12:18:56 +0900

    drawing url추가
    drawing.js 주석참고

--- WebApplication1/WebApplication1/drawing.js
+++ WebApplication1/WebApplication1/drawing.js
@@ -10,8 +10,16 @@ function publicProjectDrawing() {
             defaultStrokeWidth: 2,
             strokeWidths: [1, 2, 3, 20, 30]
         });
-        var newImage = new Image()
-        newImage.src = "https://firebasestorage.googleapis.com/v0/b/coloride1.appspot.com/o/pp1.png?alt=media&token=f130971e-5725-4563-b996-25f4e0974560"; // Modify this part to change file
+        var newImage = new Image();
+
+        var storage = firebase.storage().ref();
+
+        //storage.child(여기안에 파이어베이스 storage경로를 입력하시면 해당파일 url을 그림에 넣습니다)
+        storage.child('publicProject/sample.png').getDownloadURL().then(function (url) {
+            newImage.src = url;
+        });
+        //newImage.src = "https://firebasestorage.googleapis.com/v0/b/coloride1.appspot.com/o/pp1.png?alt=media&token=f130971e-5725-4563-b996-25f4e0974560"; // Modify this part to change file
+        
         lc.saveShape(LC.createShape('Image', { x: 10, y: 10, image: newImage }));
         var JSONstring;
         $('.controls.export [data-action=export-as-JSON]').click(function (e) {
