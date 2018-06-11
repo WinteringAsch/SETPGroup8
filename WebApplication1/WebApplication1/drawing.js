fa7fdba6a8a0eae5213815950068c76e8aefe677
Junhyeon Moon <paranocs@gmail.com>
2018-06-11 19:43:17 +0900

    privateproject 기능구현
    private projcet 기능구현

--- WebApplication1/WebApplication1/drawing.js
+++ WebApplication1/WebApplication1/drawing.js
@@ -39,4 +39,45 @@ function publicProjectCllick(num) {
     location.href = "publicProject.html?ppnum=" + num;
 }
 
+function privateProjectClick(num) {
+    location.href = "privateProject.html?pnum=" + num;
+}
+
+function privateProjectDrawing(num) {
+    $(document).ready(function () {
+        var ImageSize = { width: 489, height: 369 };
+        var ImageBound = { x: 0, y: 0, width: ImageSize.width, height: ImageSize.height };
+        var newImage = new Image();
+        /*
+        firebase.database().ref("publicProject/" + num).on('value', function (snapshot) {
+            newImage.src = snapshot.val();      // Modify this part to change file
+        });*/
+    
+        var temp = "images/gallery/gallery" + num + ".png";
+        newImage.src = temp;
+
+        var lc = LC.init(document.getElementById("lc-private"), {
+            backgroundShapes: [LC.createShape('Image', { x: 0, y: 0, image: newImage })],
+            imageSize: ImageSize,
+            imageURLPrefix: 'LC/_assets/lc-images',
+            toolbarPosition: 'bottom',
+            defaultStrokeWidth: 3,
+            strokeWidths: [1, 2, 3, 5, 10, 15, 20, 30]
+        });
+
+        //lc.saveShape(LC.createShape('Image', { x: 0, y: 0, image: newImage })); // 그림의 일부로 배경
+
+        var JSONstring;
+        $('.controls.export [data-action=export-as-PNG]').click(function (e) {
+            e.preventDefault();
+            JSONstring = JSON.stringify(lc.getSnapshot({ rect: ImageBound }));
+            firebase.database().ref("privateProject/" + num).set(lc.getImage({ rect: ImageBound }).toDataURL());
+        });
+        $('.controls.export [data-action=import-as-PNG]').click(function (e) {
+            e.preventDefault();
+            lc.loadSnapshot(JSON.parse(JSONstring));
+        });
+    });
+}
+
 
