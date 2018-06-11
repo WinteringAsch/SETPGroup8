8f17641a34b901381b097a6ca03d085ac0f7961a
Junhyeon Moon <paranocs@gmail.com>
2018-06-12 08:22:38 +0900

    그림초기화기능 추가
    그림초기화 추가

--- WebApplication1/WebApplication1/drawing.js
+++ WebApplication1/WebApplication1/drawing.js
@@ -24,14 +24,16 @@ function publicProjectDrawing(num) {
 
         var JSONstring;
         $('.controls.export [data-action=export-as-PNG]').click(function (e) {
+            window.alert("그림이 저장되었습니다");
             e.preventDefault();
             JSONstring = JSON.stringify(lc.getSnapshot({ rect: ImageBound }));
             firebase.database().ref("publicProject/" + num).set(lc.getImage({ rect: ImageBound }).toDataURL());
         });
+        /*
         $('.controls.export [data-action=import-as-PNG]').click(function (e) {
             e.preventDefault();
             lc.loadSnapshot(JSON.parse(JSONstring));
-        });
+        });*/
     });
 
 }
@@ -144,12 +146,47 @@ function initGallery(num) {
             e.preventDefault();
             JSONstring = JSON.stringify(lc.getSnapshot({ rect: ImageBound }));
             var id = $("#emailbox").html().split('@');
-            if (id[0] == '')
-                window.alert("로그인을 안하시면 저장이 안됩니다");
+            if (id[0] == '') {
+
+            }
             else {
                 firebase.database().ref("users/" + id[0] + "/" + num).set(lc.getImage({ rect: ImageBound }).toDataURL());
-                window.alert("저장 완료");
             }
         });
     });
+}
+
+function initPublicProject(num) {
+    $(document).ready(function () {
+        var ImageSize = { width: 489, height: 369 };
+        var ImageBound = { x: 0, y: 0, width: ImageSize.width, height: ImageSize.height };
+        var newImage = new Image();
+
+        var str = "images/sample" + num + ".png";
+        newImage.src = str; 
+
+        var lc = LC.init(document.getElementById("lc-public"), {
+            backgroundShapes: [LC.createShape('Image', { x: 0, y: 0, image: newImage })],
+            imageSize: ImageSize,
+            imageURLPrefix: 'LC/_assets/lc-images',
+            toolbarPosition: 'bottom',
+            defaultStrokeWidth: 3,
+            strokeWidths: [1, 2, 3, 5, 10, 15, 20, 30]
+        });
+
+        //lc.saveShape(LC.createShape('Image', { x: 0, y: 0, image: newImage })); // 그림의 일부로 배경
+
+        var JSONstring;
+        $('.controls.export [data-action=export-as-PNG]').click(function (e) {
+            e.preventDefault();
+            JSONstring = JSON.stringify(lc.getSnapshot({ rect: ImageBound }));
+            firebase.database().ref("publicProject/" + num).set(lc.getImage({ rect: ImageBound }).toDataURL());
+        });
+        /*
+        $('.controls.export [data-action=import-as-PNG]').click(function (e) {
+            e.preventDefault();
+            lc.loadSnapshot(JSON.parse(JSONstring));
+        });*/
+    });
+
 }
